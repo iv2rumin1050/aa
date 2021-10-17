da601b42d6d71f6289195ba317aca10e3b860e2e
iv2rumin1050 <92675429+iv2rumin1050@users.noreply.github.com>
2021-10-17 18:21:18 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,3 +1,10 @@
 from selenium import webdriver
+import time
+
 driver = webdriver.Firefox(executable_path='/home/colab/geckodriver')
-driver.get("http://www.google.com")
+driver.get("https://story-ify.blogspot.com/2021/10/make-1250-worldwide-for-each-image-make.html")
+time.sleep(15)
+driver.get("https://story-ify.blogspot.com/2021/09/earn-100-every-5-minutes-in-free-paypal.html")
+time.sleep(5)
+clk = driver.find_element_by_xpath("/html/body/div/div[2]/div/a/img")
+clk.click()
